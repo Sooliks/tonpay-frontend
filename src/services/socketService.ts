96021d195867d8957d076bd13a356656f0f512c5
Sooliks <evgenij.terexov.2000@mail.ru>
2024-10-21 00:18:52 +0300

    add notifications with socket io

--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -3,7 +3,7 @@ import io, {Socket} from "socket.io-client";
 export class SocketService {
     static socket: null | Socket = null;
     static createConnection() {
-        this.socket = io(process.env.MODE === 'PRODUCTION' ? 'https://sooliks-tonpay-backend-8ff0.twc1.net' : 'http://localhost:4200', {
+        this.socket = io(process.env.NODE_ENV === 'production' ? 'https://sooliks-tonpay-backend-8ff0.twc1.net' : 'http://localhost:4200', {
             auth: { token: localStorage.getItem("token")},
         });
 
