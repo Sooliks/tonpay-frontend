aca9da9b0dde8f5648ec765a01747103d794519e
Sooliks <evgenij.terexov.2000@mail.ru>
2024-11-04 15:28:25 +0300

    add sales boost

--- src/app/profile/sales/layout.tsx
+++ src/app/profile/sales/layout.tsx
@@ -1,16 +1,41 @@
 'use client'
-import React from 'react';
+import React, {useState} from 'react';
 import Link from "next/link";
 import {Button} from "@/components/ui/button";
 import {usePathname} from "next/navigation";
+import {Loader2} from "lucide-react";
+import axiosInstance from "@/configs/axios";
+import {toast} from "@/hooks/use-toast";
+import {AxiosError} from "axios";
 
 const SalesProfileLayout = ({children}:{children: React.ReactNode}) => {
     const pathname = usePathname()
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const handleBoostSales = () => {
+        setIsLoading(true)
+        axiosInstance.post('/sales/up').then(res=>{
+            if(res.status === 201) {
+                toast({description: `Sales boosted`})
+            }
+        }).finally(() => setIsLoading(false)).catch((error: AxiosError)=>{
+            const errorMessage = (error.response?.data as { message?: string })?.message || error.message;
+            toast({description: errorMessage})
+        })
+    }
     return (
         <div className={'p-4'}>
-            <Link href={pathname === '/profile/sales/add' ? '/profile/sales' : '/profile/sales/add'}>
-                <Button variant={'secondary'}>{pathname === '/profile/sales/add' ? 'Back' : 'Add new'}</Button>
-            </Link>
+            <div className={'flex justify-between items-center'}>
+                <Link href={pathname === '/profile/sales/add' ? '/profile/sales' : '/profile/sales/add'}>
+                    <Button variant={'secondary'}>{pathname === '/profile/sales/add' ? 'Back' : 'Add new'}</Button>
+                </Link>
+                <Button
+                    disabled={isLoading}
+                    onClick={handleBoostSales}
+                >
+                    {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                    Boost sales
+                </Button>
+            </div>
             {children}
         </div>
     );
