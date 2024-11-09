96c08c4b96370e7d68818dd801fc51292c22d902
Sooliks <evgenij.terexov.2000@mail.ru>
2024-11-09 16:43:03 +0300

    fix

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? "https://sooliks-tonpay-backend-8ff0.twc1.net/api" : 'http://localhost:4200/api'
+    baseURL: process.env.NODE_ENV === 'production' ? "https://sooliks-tonpay-backend-5cf7.twc1.net/api" : 'http://localhost:4200/api'
 })
 
 axiosInstance.interceptors.request.use(
--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -2,7 +2,7 @@ import io, {Socket} from "socket.io-client";
 
 export class SocketService {
     static createConnection(namespace: string) {
-        const socket: Socket = io(process.env.NODE_ENV === 'production' ? 'https://sooliks-tonpay-backend-8ff0.twc1.net/' + namespace : 'http://localhost:4200/' + namespace, {
+        const socket: Socket = io(process.env.NODE_ENV === 'production' ? 'https://sooliks-tonpay-backend-5cf7.twc1.net/' + namespace : 'http://localhost:4200/' + namespace, {
             auth: { token: localStorage.getItem("token")},
         });
         socket.on('connect', () => {
