806a44de58198426c96f6d7615ffd4add1d702f9
Sooliks <evgenij.terexov.2000@mail.ru>
2024-11-12 22:41:16 +0300

    fgfg

--- src/app/profile/settings/page.tsx
+++ src/app/profile/settings/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {ThemeToggle} from "@/components/ThemeToggle";
+import CopyButton from "@/components/my-ui/CopyButton";
+import {useAuth} from "@/hooks/useAuth";
 
 const SettingsPage = () => {
+    const auth = useAuth()
     return (
         <div className={'p-4'}>
             <h4 className={'text-center scroll-m-20 text-xl font-semibold tracking-tight'}>Settings</h4>
@@ -9,6 +12,7 @@ const SettingsPage = () => {
                 <h6>Theme</h6>
                 <ThemeToggle/>
             </div>
+            <CopyButton className={'mt-4'} copyText={`https://t.me/PayOnTonBot/app?startapp=${auth.user!.id}`} textButton={'Referral link'}/>
         </div>
     );
 };
