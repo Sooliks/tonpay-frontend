ac6ccff91512f58650ae91b7090d8b52db7911af
Sooliks <evgenij.terexov.2000@mail.ru>
2024-11-12 16:33:50 +0300

    add count for orders

--- src/app/sale/[id]/BuyMenu.tsx
+++ src/app/sale/[id]/BuyMenu.tsx
@@ -17,14 +17,20 @@ import {AxiosError} from "axios";
 import {Loader2} from "lucide-react";
 import {useRouter} from "next/navigation";
 import useTonPrice from "@/hooks/useTonPrice";
+import {Input} from "@/components/ui/input";
 
 const BuyMenu = ({sale}:{sale: Sale}) => {
     const [isLoading, setIsLoading] = useState(false);
+    const [count,setCount] = useState(1);
     const {priceTon} = useTonPrice()
     const {push} = useRouter();
     const handleSubmit = () => {
+        if(count < 1){
+            toast({description: 'The quantity must be greater than zero'})
+            return;
+        }
         setIsLoading(true)
-        axiosInstance.post(`/orders`, {saleId: sale.id, price: sale.price}).then(res=>{
+        axiosInstance.post(`/orders`, {saleId: sale.id, price: sale.price, count: count}).then(res=>{
             if(res.status === 201) {
                 toast({description: 'Order created'})
                 push(`/chat/${res.data.chatId}`)
@@ -37,7 +43,7 @@ const BuyMenu = ({sale}:{sale: Sale}) => {
     return (
         <Drawer>
             <DrawerTrigger asChild>
-                <Button>Buy <span className={'text-muted-foreground text-sm ml-2'}>[{sale.price} TON] ~ {(priceTon * sale.price).toFixed(2)}$</span></Button>
+                <Button>Buy <span className={'text-muted-foreground text-sm ml-2'}>[{sale.price * count} TON] ~ {(priceTon * (sale.price * count)).toFixed(2)}$</span></Button>
             </DrawerTrigger>
             <DrawerContent>
                 <div className="mx-auto w-full max-w-sm">
@@ -45,12 +51,21 @@ const BuyMenu = ({sale}:{sale: Sale}) => {
                         <DrawerTitle>Buy {sale.title}</DrawerTitle>
                     </DrawerHeader>
                     <DrawerFooter>
+                        <label htmlFor="count" className={'text-muted-foreground text-sm'}>Count</label>
+                        <Input
+                            placeholder={'Count'}
+                            id={'count'}
+                            value={count}
+                            type={'number'}
+                            min={1}
+                            onChange={(e)=>setCount(Number(e.target.value))}
+                        />
                         <Button
                             onClick={handleSubmit}
                             disabled={isLoading}
                         >
                             {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-                            Buy <span className={'text-muted-foreground text-sm ml-2'}>[{sale.price} TON] ~ {(priceTon * sale.price).toFixed(2)}$</span>
+                            Buy <span className={'text-muted-foreground text-sm ml-2'}>[{sale.price * count} TON] ~ {(priceTon * (sale.price * count)).toFixed(2)}$</span>
                         </Button>
                         <DrawerClose asChild>
                             <Button variant="outline">Cancel</Button>
