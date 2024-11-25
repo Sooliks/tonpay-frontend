7ef3eb669267f0e1becfb35d9c33dc0220910b1d
Sooliks <evgenij.terexov.2000@mail.ru>
2024-11-25 20:25:36 +0300

    add color frame

--- src/app/[lang]/profile/settings/page.tsx
+++ src/app/[lang]/profile/settings/page.tsx
@@ -11,11 +11,10 @@ const SettingsPage = () => {
     const {translations} = useTranslation()
     return (
         <div className={'p-4'}>
-            <h4 className={'text-center scroll-m-20 text-xl font-semibold tracking-tight'}>{translations.profile.settings.title}</h4>
-            <div>
-                <h6>{translations.profile.settings.theme}</h6>
-                <ThemeToggle/>
-            </div>
+            <Card className={'p-4'}>
+                <h4 className={'text-center scroll-m-20 text-xl font-semibold tracking-tight'}>{translations.profile.settings.title}</h4>
+            </Card>
+            <ThemeToggle/>
             <Card className={'flex flex-col items-center p-4 mt-4'}>
                 <CopyButton refTelegram={false} copyText={`https://t.me/PayOnTonBot/app?startapp=${auth.user?.id}`} textButton={translations.profile.settings.refButton}/>
                 <p className={'text-sm text-muted-foreground text-center'}>{translations.frequent.soon}</p>
