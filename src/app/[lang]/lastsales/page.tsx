277af1d3c6cb944ede6ff7fe399495633ffb471c
Sooliks <evgenij.terexov.2000@mail.ru>
2024-12-21 13:41:28 +0300

    fix

--- src/app/[lang]/lastsales/page.tsx
+++ src/app/[lang]/lastsales/page.tsx
@@ -14,7 +14,7 @@ const LastSalesPage = () => {
         <div className={'p-4'}>
             {/*<h4 className={'text-center scroll-m-20 text-xl font-semibold tracking-tight'}>{translations.menu.main.last20Sales}</h4>*/}
             {data && data.length > 0 ? data.map(sale =>
-                    <SalePreview sale={sale} key={sale.id} avatar={true} isProfile={false} rate/>
+                    <SalePreview sale={sale} key={sale.id} avatar={true} isProfile={true} rate/>
                 )
                 :
                 <p className={'text-center text-muted-foreground text-2xl mt-24'}>Nothing</p>
