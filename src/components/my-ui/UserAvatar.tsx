8946d3511a6986e275fd67c1eca03de1dfc01ec5
Sooliks <evgenij.terexov.2000@mail.ru>
2025-01-02 19:47:30 +0300

    fix

--- src/components/my-ui/UserAvatar.tsx
+++ src/components/my-ui/UserAvatar.tsx
@@ -1,18 +1,33 @@
 'use client'
-import React from 'react';
+import React, {useState} from 'react';
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 import Link from "next/link";
 import {cn} from "@/lib/utils";
+import Image from "next/image";
 
 const UserAvatar = ({photoUrl, nickname, id, link = true, small = false, className}: {photoUrl: string, nickname: string, id: string, link?: boolean, small?: boolean, className?: string}) => {
+    const [imageError, setImageError] = useState(false);
+
     return (
         link ?
             <div className={cn(className, 'flex items-center')}>
                 <Link href={`/profile/${id}`}>
-                    <Avatar className={cn(!small ? 'h-6 w-6' : 'h-4 w-4', 'z-0')}>
+                    {!imageError ?
+                        <Image
+                            src={photoUrl}
+                            alt={nickname[0]}
+                            className={cn(!small ? 'h-6 w-6' : 'h-4 w-4', 'z-0 rounded-full')}
+                            onError={() => setImageError(true)}
+                        />
+                        :
+                        <div className={cn(!small ? 'h-6 w-6' : 'h-4 w-4', 'z-0 rounded-full bg-gray-300')}>
+                            {nickname[0]}
+                        </div>
+                    }
+                    {/*<Avatar className={cn(!small ? 'h-6 w-6' : 'h-4 w-4', 'z-0')}>
                         <AvatarImage src={photoUrl}/>
                         <AvatarFallback>{nickname[0]}</AvatarFallback>
-                    </Avatar>
+                    </Avatar>*/}
                 </Link>
                 <Link href={`/profile/${id}`} className={'text-muted-foreground text-sm ml-2'}>
                     @{nickname}
@@ -20,10 +35,22 @@ const UserAvatar = ({photoUrl, nickname, id, link = true, small = false, classNa
             </div>
             :
             <div className={cn(className, 'flex items-center')}>
-                <Avatar className={cn(!small ? 'h-6 w-6' : 'h-4 w-4', 'z-0')}>
+                {!imageError ?
+                    <Image
+                        src={photoUrl}
+                        alt={nickname[0]}
+                        className={cn(!small ? 'h-6 w-6' : 'h-4 w-4', 'z-0 rounded-full')}
+                        onError={() => setImageError(true)}
+                    />
+                    :
+                    <div className={cn(!small ? 'h-6 w-6' : 'h-4 w-4', 'z-0 rounded-full bg-gray-300')}>
+                        {nickname[0]}
+                    </div>
+                }
+                {/*<Avatar className={cn(!small ? 'h-6 w-6' : 'h-4 w-4', 'z-0')}>
                     <AvatarImage src={photoUrl}/>
                     <AvatarFallback>{nickname[0]}</AvatarFallback>
-                </Avatar>
+                </Avatar>*/}
                 <p className={'text-muted-foreground text-sm ml-2'}>@{nickname}</p>
             </div>
     );
