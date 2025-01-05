fb522a6c46d30ead1fdc9ec2c89ce256e59ea005
Sooliks <evgenij.terexov.2000@mail.ru>
2025-01-05 20:55:47 +0300

    fix

--- src/app/[lang]/profile/settings/page.tsx
+++ src/app/[lang]/profile/settings/page.tsx
@@ -11,7 +11,7 @@ const SettingsPage = () => {
             <Card className={'p-4'}>
                 <h4 className={'text-center scroll-m-20 text-xl font-semibold tracking-tight'}>{translations.profile.settings.title}</h4>
             </Card>
-            <Card className={'p-4 w-full h-full'}>
+            <Card className={'p-4 w-full h-full mt-2'}>
                 <Card className={'p-4 flex items-center w-40'}>
                     <p className={'mr-2 text-sm text-muted-foreground'}>Theme</p>
                     <ThemeToggle/>
